0a8a0a29bd5e6fc457f8c5e465ac56fcc988a8ea
Sanyi Egyed <e.sanyi@gmail.com>
2013-05-28 21:25:34 +0200

    First attempt

---
+++ Alakzatok.java
@@ -0,0 +1,36 @@
+public abstract Alakzat
+{
+
+private int x;
+private int y;
+
+
+	public void Add()
+	{
+	
+	}
+
+	ppublic void Search()
+	{
+		try{}
+		catch() {}
+	}
+
+	public abstract void terulet();
+	public abstract void kerulet();
+
+}
+
+public Negyzet extends Alakzat
+{
+
+
+}
+public Teglalap extends Alakzat
+{
+
+}
+public Rombusz extends Alakzat
+{
+
+}
